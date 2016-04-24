d5362c7a2b9572ee9c8f3e967478c6bf87a1c409
boazshor <boaz.shor@hp.com>
2016-04-24 11:58:12 +0300

    tests continued

--- spec/inputs/kafka_then_s3_spec.rb
+++ spec/inputs/kafka_then_s3_spec.rb
@@ -2,6 +2,11 @@
 # require "logstash/spec/spec_helper"
 require "logstash/inputs/kafka_then_s3"
 require 'jruby-kafka'
+require "logstash/errors"
+require_relative "../support/helpers"
+require "stud/temporary"
+require "aws-sdk"
+require "fileutils"
 
 class LogStash::Inputs::TestKafkaThenS3 < LogStash::Inputs::KafkaThenS3
     private
@@ -35,6 +40,15 @@ class LogStash::Inputs::TestKafkaThenS3 < LogStash::Inputs::KafkaThenS3
     def queue_event(msg, output_queue)
       super(msg, output_queue)
     end
+
+    def each
+      counter = 1
+
+      loop do
+        yield "awesome-#{counter}"
+        counter +=1
+      end
+    end
   end
 
   class TestInfiniteKafkaGroup < Kafka::Group
@@ -49,16 +63,60 @@ class LogStash::Inputs::TestKafkaThenS3 < LogStash::Inputs::KafkaThenS3
     end
   end
 
-  describe LogStash::Inputs::TestKafkaThenS3 do
-    let (:kafka_config) {{'topic_id' => 'test','bucket' => 'test_bucket'}}
-    let (:empty_config) {{'bucket' => 'test_bucket'}}
-    let (:bad_kafka_config) {{'topic_id' => 'test', 'white_list' => 'other_topic','bucket' => 'test_bucket'}}
-    let (:white_list_kafka_config) {{'white_list' => 'other_topic','bucket' => 'test_bucket'}}
-    #let (:decorated_kafka_config) {{'topic_id' => 'test', 'decorate_events' => true}}
 
+
+  describe LogStash::Inputs::TestKafkaThenS3 do
+       before do
+         FileUtils.mkdir_p(sincedb_path)
+         AWS.stub!
+         Thread.abort_on_exception = true
+       end
+       context "when interrupting the plugin" do
+         let(:config) { super.merge({ "interval" => 5 }) }
+
+         before do
+           expect_any_instance_of(LogStash::Inputs::KafkaThenS3).to receive(:list_new_files).and_return(TestKafkaThenS3.new)
+         end
+
+         it_behaves_like "an interruptible input plugin"
+       end
+
+
+
+       after do
+         delete_remote_files(prefix)
+         FileUtils.rm_rf(temporary_directory)
+         delete_remote_files(backup_prefix)
+       end
+
+
+    let (:kafka_config) {{'topic_id' => 'test',
+                          'bucket' => 'test_bucket',
+                          "access_key_id" => "1234",
+                          "secret_access_key" => "secret",
+                          "bucket" => "logstash-test",
+                          "temporary_directory" => temporary_directory,
+                          "sincedb_path" => File.join(sincedb_path, ".sincedb")
+    }}
+    let (:min_config) {{'bucket' => 'test_bucket'}}
+    let (:bad_kafka_config) {{'topic_id' => 'test',
+                              'white_list' => 'other_topic',
+                              'bucket' => 'test_bucket'
+    }}
+    let (:white_list_kafka_config) {{'white_list' => 'other_topic','bucket' => 'test_bucket'}}
+
+    #let (:decorated_kafka_config) {{'topic_id' => 'test', 'decorate_events' => true}}
+    let(:temporary_directory) { Stud::Temporary.pathname }
+    let(:sincedb_path) { Stud::Temporary.pathname }
+    let(:day) { 3600 * 24 }
+    let(:temporary_directory) { Stud::Temporary.pathname }
+    let(:prefix)  { 'logstash-s3-input-prefix/' }
     it "should register" do
+      FileUtils.rm_rf(temporary_directory)
       input = LogStash::Plugin.lookup("input", "kafka_then_s3").new(kafka_config)
       expect {input.register}.to_not raise_error
+      # expect { input.register }.to change { Dir.exist?(temporary_directory) }.from(false).to(true)
+      FileUtils.rm_rf(temporary_directory)
     end
 
     it "should register with whitelist" do
@@ -67,49 +125,51 @@ class LogStash::Inputs::TestKafkaThenS3 < LogStash::Inputs::KafkaThenS3
     end
 
     it "should fail with multiple topic configs" do
-      input = LogStash::Plugin.lookup("input", "kafka_then_s3").new(empty_config)
-      expect {input.register}.to raise_error
-    end
-
-    it "should fail without topic configs" do
       input = LogStash::Plugin.lookup("input", "kafka_then_s3").new(bad_kafka_config)
       expect {input.register}.to raise_error
     end
 
-    # it_behaves_like "an interruptible input plugin" do
-    #   let(:config) { kafka_config }
-    #   let(:mock_kafka_plugin) { LogStash::Inputs::TestInfiniteKafka.new(config) }
-    #
-    #   before :each do
-    #     allow(LogStash::Inputs::TestKafkaThenS3).to receive(:new).and_return(mock_kafka_plugin)
-    #     expect(subject).to receive(:create_consumer_group) do |options|
-    #       TestInfiniteKafkaGroup.new(options)
-    #     end
-    #   end
-    # end
+    it "should fail without topic configs" do
+      input = LogStash::Plugin.lookup("input", "kafka_then_s3").new(min_config)
+      expect {input.register}.to raise_error
+    end
+
+
+
+    it_behaves_like "an interruptible input plugin" do
+      let(:config) { kafka_config }
+      let(:mock_kafka_plugin) { LogStash::Inputs::TestInfiniteKafka.new(config) }
+
+      before :each do
+        allow(LogStash::Inputs::TestKafkaThenS3).to receive(:new).and_return(mock_kafka_plugin)
+        expect(subject).to receive(:create_consumer_group) do |options|
+          TestInfiniteKafkaGroup.new(options)
+        end
+      end
+    end
 
     it 'should populate kafka config with default values' do
-      kafka = LogStash::Inputs::TestKafka.new(kafka_config)
+      kafka = LogStash::Inputs::TestKafkaThenS3.new(kafka_config)
       insist {kafka.zk_connect} == 'localhost:2181'
       insist {kafka.topic_id} == 'test'
       insist {kafka.group_id} == 'logstash'
       !insist { kafka.reset_beginning }
     end
 
-    it 'should retrieve event from kafka' do
-      kafka = LogStash::Inputs::TestKafka.new(kafka_config)
-      expect(kafka).to receive(:create_consumer_group) do |options|
-        TestKafkaGroup.new(options)
-      end
-      kafka.register
-
-      logstash_queue = Queue.new
-      kafka.run logstash_queue
-      e = logstash_queue.pop
-      insist { e['message'] } == 'Kafka message'
-      # no metadata by default
-      insist { e['kafka'] } == nil
-    end
+    # it 'should retrieve event from kafka' do
+    #   kafka = LogStash::Inputs::TestKafkaThenS3.new(kafka_config)
+    #   expect(kafka).to receive(:create_consumer_group) do |options|
+    #     TestKafkaGroup.new(options)
+    #   end
+    #   kafka.register
+    #
+    #   logstash_queue = Queue.new
+    #   kafka.run logstash_queue
+    #   e = logstash_queue.pop
+    #   insist { e['message'] } == 'Kafka message'
+    #   # no metadata by default
+    #   insist { e['kafka'] } == nil
+    # end
 
 
   end
---
+++ spec/support/helpers.rb
@@ -0,0 +1,46 @@
+# def fetch_events(settings)
+#   queue = []
+#   s3 = LogStash::Inputs::KafkaThenS3.new(settings)
+#   s3.register
+#   #need to define event
+#   s3.process_files(event , queue)
+#   queue
+# end
+
+# delete_files(prefix)
+def upload_file(local_file, remote_name)
+  bucket = s3object.buckets[ENV['AWS_LOGSTASH_TEST_BUCKET']]
+  file = File.expand_path(File.join(File.dirname(__FILE__), local_file))
+  bucket.objects[remote_name].write(:file => file)
+end
+
+def delete_remote_files(prefix)
+  bucket = s3object.buckets[ENV['AWS_LOGSTASH_TEST_BUCKET']]
+  bucket.objects.with_prefix(prefix).each { |object| object.delete }
+end
+
+def list_remote_files(prefix, target_bucket = ENV['AWS_LOGSTASH_TEST_BUCKET'])
+  bucket = s3object.buckets[target_bucket]
+  bucket.objects.with_prefix(prefix).collect(&:key)
+end
+
+def delete_bucket(name)
+  s3object.buckets[name].objects.map(&:delete)
+  s3object.buckets[name].delete
+end
+
+def s3object
+  AWS::S3.new
+end
+
+# class TestInfiniteS3Object
+#   def each
+#     counter = 1
+#
+#     loop do
+#       yield "awesome-#{counter}"
+#       counter +=1
+#     end
+#   end
+# end
+
